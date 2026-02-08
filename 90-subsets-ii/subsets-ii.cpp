f5e6520e99cdf7eabf31a0ae1fe0b1869131f339
Devang Mate <matedevang6308@gmail.com>
2026-02-08 13:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (84.58%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-void findpowset(int index,vector<int>& ds,vector<vector<int>>& ans,vector<int>& arr){
- if(index==arr.size()){
-    ans.push_back(ds);
-    return;
- }
-
- if(ds.empty() || ds.back()!=arr[index]){
-    findpowset(index+1,ds,ans,arr);
-    
- }
- ds.push_back(arr[index]);
- findpowset(index+1,ds,ans,arr);
- ds.pop_back();
- 
- 
-
-}
+    void findpowsetopt(int start, vector<int>& current,
+                    vector<vector<int>>& result, vector<int>& arr) {
+        result.push_back(current);
+        for (int i = start; i < arr.size(); i++) {
+            if (i > start && arr[i - 1] == arr[i])
+                continue;
+            current.push_back(arr[i]);
+            findpowsetopt(i + 1, current, result, arr);
+            current.pop_back();
+        }
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
-    vector<int> ds;
-    sort(nums.begin(),nums.end());
-    findpowset(0,ds,ans,nums);
-    return ans;
+        vector<int> ds;
+        sort(nums.begin(), nums.end());
+        findpowsetopt(0, ds, ans, nums);
+        return ans;
     }
 };
